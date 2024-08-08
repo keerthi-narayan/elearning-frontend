69bdfc3a19be29b041fb5374e97a1d9aa7201f68
keerthi-narayan <keerthinarayan.2002@gmail.com>
2024-08-08 13:33:12 +0530

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './footer.css';
+import { IoLogoFacebook } from "react-icons/io5";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { BsInstagram } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-content">
+            <p>
+                &copy; 2024 Your E-Learning Platform. All rights reserved. <br />
+                Made with ❤️ <a href=''>Keerthi N</a>
+            </p>
+
+            <div className="social-links">
+                <a href=''><IoLogoFacebook/></a>
+                <a href=''><AiFillTwitterCircle/></a>
+                <a href=''><BsInstagram/></a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className='about-content'>
+            <h2> About us</h2>
+            <p>
+                Welcome to our E-Learning Platform , 
+                where we are dedicated to transforming education through innovative 
+                and engaging online learning experiences.Our mission is to provide high-quality, accessible,
+                and flexible education that empowers learners worldwide to achieve their personal and professional goals.
+                Whether you're looking to advance your career, learn a new skill, or explore a new field of interest, 
+                we are here to support you every step of the way. Join us today and start your path to success!
+            </p>
+        </div>    
+    </div>
+  )
+}
+
+export default About
